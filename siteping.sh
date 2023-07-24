071bc555e4dad4abee5c3135dfa6b0e9702ebad7
Seizu <seizu@gmx.at>
2023-07-24 20:22:56 +0200

    Update siteping.sh
    Search for " 0% packet loss", any other text triggers a notification

--- siteping.sh
+++ siteping.sh
@@ -20,16 +20,17 @@ while true
 do
  for (( i=0; i<${size}; i+=2 ));
  do
-  x1=$(ping ${array[$i]} -c 1 -W $timeout 2>/dev/null | grep -c "100% packet loss") 
-  if [ "${array[$i+1]}" -lt $count -a $x1 -eq 0 ]; then
-    $path/tpush.sh "Server '"${array[$i]}"' is Up" 
+  x1=$(ping ${array[$i]} -c 1 -W $timeout 2>/dev/null | grep -c " 0% packet loss")
+  if [ "${array[$i+1]}" -lt $count -a $x1 -eq 1 ]; then
+    $path/tpush.sh "Server '"${array[$i]}"' is Up"
     array[$i+1]=$count
   else
-    if [ "${array[$i+1]}" -gt 0 -a $x1 -eq 1 ]; then 
-      $path/tpush.sh "Server '"${array[$i]}"' is Down" 
+    if [ "${array[$i+1]}" -gt 0 -a $x1 -eq 0 ]; then
+      $path/tpush.sh "Server '"${array[$i]}"' is Down"
       array[$i+1]=$((${array[$i+1]} - 1))
     fi
   fi
  done
 sleep $interval
 done
+
