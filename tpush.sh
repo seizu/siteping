0459f349616caeb6bcaef4ef55aac4604f82fcb2
seizu <seizu@gmx.at>
2023-08-18 01:25:32 +0200

    refactoring

--- tpush.sh
+++ tpush.sh
@@ -10,5 +10,4 @@ token="YOUR_TOKEN"
 chatId="YOUR_CHAT_ID"
 
 curl --connect-timeout $timeout -G -s -o /dev/null "https://api.telegram.org/bot$token/sendMessage?" --data-urlencode "text=$1" --data-urlencode "chat_id=$chatId" || exit 1
-
 exit 0
